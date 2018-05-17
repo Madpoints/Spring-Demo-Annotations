7872feb90a2c024cf2d157ac9fd7d14bfed9ebe8
John Furtado <jf8846@gmail.com>
2018-05-17 18:59:55 -0400

    spring config with java code

---
+++ src/com/madpoints/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,26 @@
+package com.madpoints.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config java class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(SportConfig.class);
+		
+		// get bean from container
+		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		
+		// call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		
+		
+		// close context
+		context.close();
+		
+	}
+
+}
---
+++ src/com/madpoints/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.madpoints.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.madpoints.springdemo")
+public class SportConfig {
+
+}
